d5f7a1c0c451fb177575b14fb96ccbc6a00a4dd0
tpodsosony <talpods6@gmail.com>
2024-12-06 15:11:31 -0600

    Added keys for stripe

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,4 @@
+Rails.configuration.stripe[:publishable_key] = 'pk_test_51QT8EJHlUGNWkPskpx9VyUZAW0QKNiYNcCLzzDubhjT2ZBRfolF4NHxg08pmFb2W2tqiXntAu2YoXfsqiWNMBecn00Fh9hqh8Y'
+Rails.configuration.stripe[:secret_key] = 'sk_test_51QT8EJHlUGNWkPskp4RRScRm24xsXXIPTLJVJapLMf3ksX6rTeAQ1jwMcpQaT5UwUoF86zBXDdv0IHVnPBMsc1ZU00PzLUC3kU'
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
