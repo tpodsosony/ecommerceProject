8d8cd4ca9ea7fd36cefafe26587053557af49c83
tpodsosony <talpods6@gmail.com>
2024-12-06 15:37:14 -0600

    Added buy now button to cart

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   resources :cart
 
   scope "/checkout" do
-    post "create". to: "checkout#create", as: "checkout_create"
-    get "success", toL "checkout#success", as: "checkout_success"
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
     get "cancel", to: "checkout#cancel", as: "checkout_cancel"
   end
 
