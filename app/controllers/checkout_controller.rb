efd237686a9d0f13d89636a83e33738b35cd05b1
tpodsosony <talpods6@gmail.com>
2024-12-13 11:26:34 -0600

    Got checkout working with stripe

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,9 +1,29 @@
 class CheckoutController < ApplicationController
 
-    
+
     def create
         #Fetch the product in question!
-        product = Product.find(params[:product_id])
+        @session = Stripe::Checkout::Session.create(
+            payment_method_types: ["card"],
+            success_url: checkout_success_url,
+            cancel_url: checkout_cancel_url,
+            mode: "payment",
+            line_items: cart.map do |item| 
+                {
+                    price_data: {
+                        currency: "cad",
+                        product_data: {
+                            name: item[:product].name,
+                            description: item[:product].description,
+
+                        },
+                    unit_amount: (item[:product].price * 100).to_i,
+                    },
+                    quantity: item[:quantity]
+                }
+            end
+        )
+        redirect_to @session.url, allow_other_host: true
     end
 
     def success
